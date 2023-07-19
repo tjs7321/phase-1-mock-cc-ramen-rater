281a0c0200877da0085f9338c9d0f8358988b1e2
Theodore Smith <tjs7321@gmail.com>
2023-07-19 15:28:52 -0400

    finished minimum deliverables

--- src/index.js
+++ src/index.js
@@ -5,7 +5,43 @@ const bigName = document.getElementById("big-name")
 const bigRestaurant = document.getElementById("big-restaurant")
 const bigRating = document.getElementById("rating-display")
 const bigComment = document.getElementById("comment-display")
+const newRamenForm = document.getElementById("new-ramen")
+const newName = document.getElementById("new-name")
+const newRestaurant = document.getElementById("new-restaurant")
+const newImage = document.getElementById("new-image")
+const newRating = document.getElementById("new-rating")
+const newComment = document.getElementById("new-comment")
 
+
+newRamenForm.onsubmit = (e) => {
+    e.preventDefault()
+    let newRamen = {
+        name: newName.value,
+        restaurant: newRestaurant.value,
+        image: newImage.value,
+        rating: newRating.value,
+        comment: newComment.value
+    }
+    const postNewRamen = {
+        method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify(newRamen)
+    }
+    fetch(baseURL, postNewRamen)
+    .then(response => {return response.json()})
+    .then(ramen => {
+        ramenThumbnail = document.createElement("img")
+        ramenThumbnail.src = ramen.image
+        ramenThumbnail.onclick = () => {
+            centerRamenImage(ramen.id)
+        }
+        ramenMenu.appendChild(ramenThumbnail)
+    })
+    newRamenForm.reset()
+}
 
 /////on page load, request all ramen from server/////
 document.addEventListener("DOMContentLoaded", () => {
@@ -21,13 +57,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 centerRamenImage(ramen.id)
             }
             ramenMenu.appendChild(ramenThumbnail)
-            console.log(ramen.id)
         }
     }
 })
 const centerRamenImage = (ramenId) => {
-    console.log(ramenId)
-    console.log("http://localhost:3000/ramens" + '/' + ramenId)
     fetch("http://localhost:3000/ramens" + '/' + ramenId)
     .then(response => response.json())
     .then(ramen => {
@@ -36,6 +69,5 @@ const centerRamenImage = (ramenId) => {
         bigRestaurant.innerText = ramen.restaurant
         bigRating.innerText = ramen.rating
         bigComment.innerText = ramen.comment
-        console.log(ramen)
     })
 }
